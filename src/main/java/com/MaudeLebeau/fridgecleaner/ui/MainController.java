ee8670db74bb0ee6e460aa90bbe6953db6768400
Maude Lebeau <maude.lebeau.1@ulaval.ca>
2025-12-28 03:15:10 -0500

    feature(Inventaire): delete/add buttons on inventaire window

--- src/main/java/com/MaudeLebeau/fridgecleaner/ui/MainController.java
+++ src/main/java/com/MaudeLebeau/fridgecleaner/ui/MainController.java
@@ -1,15 +1,9 @@
 package com.MaudeLebeau.fridgecleaner.ui;
 
-import com.MaudeLebeau.fridgecleaner.domain.Unit;
-import com.MaudeLebeau.fridgecleaner.repository.ItemRepository;
-import com.MaudeLebeau.fridgecleaner.service.ItemService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 
 public class MainController {
